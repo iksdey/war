7a5b9b71f346658498ce4c1de5578130ad2ee4fe
iksdey <alexandre.gengoul@gmail.com>
2026-05-28 07:03:45 +0200

    Card now implements Comparable

--- src/main/java/com/gengoul/war/domain/Card.java
+++ src/main/java/com/gengoul/war/domain/Card.java
@@ -1,9 +1,17 @@
 package com.gengoul.war.domain;
 
-public record Card(Suit suit, Rank value) {
+public record Card(Suit suit, Rank rank) implements Comparable<Card> {
 
     @Override
     public String toString() {
-        return value.getSymbol() + suit.getSymbol();
+        return rank.getSymbol() + suit.getSymbol();
+    }
+
+    @Override
+    public int compareTo(Card other) {
+        return Integer.compare(
+                this.rank.getValue(),
+                other.rank.getValue()
+        );
     }
 }
