85cab13a54287a1836a5e4b33b5148d002da0e94
iksdey <alexandre.gengoul@gmail.com>
2026-05-29 05:33:12 +0200

    multi-line cards display and round render

--- src/main/java/com/gengoul/war/ui/ConsoleRenderer.java
+++ src/main/java/com/gengoul/war/ui/ConsoleRenderer.java
@@ -1,21 +1,50 @@
 package com.gengoul.war.ui;
 
 import com.gengoul.war.domain.Card;
+import com.gengoul.war.domain.Player;
 import com.gengoul.war.domain.RoundResult;
+import com.gengoul.war.domain.RoundStep;
 
 import java.util.List;
+import java.util.Map;
 
 public class ConsoleRenderer {
 
+    private static final int MAX_CHARACTERS_PER_LINE = 100;
+    private static final String CARD_SEPARATOR = "  ";
+
     public void displayCards(List<Card> cards) {
+        int charCount = 0;
         for (Card card : cards) {
-            System.out.print(card + "  ");
+            if (charCount > MAX_CHARACTERS_PER_LINE) {
+                System.out.println();
+                charCount = 0;
+            }
+            charCount = charCount + CARD_SEPARATOR.length() + 2;
+            System.out.print(card + CARD_SEPARATOR);
         }
+        System.out.println();
         System.out.println();
     }
 
     public void displayRound(RoundResult round) {
-        // TODO
-        System.out.println("Le round en question :" + round);
+        List<RoundStep> steps = round.steps();
+        for (int i=0; i<steps.size(); i++) {
+            if (i > 0) {
+                System.out.println("BATAILLE !");
+            }
+            for (Map.Entry<Player, Card> entry : steps.get(i).playedCards().entrySet()) {
+                System.out.println(entry.getKey().getName() + " -> " + entry.getValue());
+            }
+        }
+
+        Player winner = round.winner();
+        if (winner != null) {
+            System.out.println(round.winner().getName() + " remporte le tour");
+        } else {
+            System.out.println("Personne n'a gagné le tour");
+        }
+
+        System.out.println();
     }
 }
